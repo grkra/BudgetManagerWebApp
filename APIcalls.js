5e1003b14bfcbba2e6a52453707e382f6bf795d6
Grzegorz Krawczyk <grzegorz.krawczyk.programista@gmail.com>
2025-01-17 20:34:04 +0100

    changed src of validate script to https

---
+++ APIcalls.js
@@ -0,0 +1,57 @@
+async function getLimit(selectedCategory) {
+    try {
+        const response = await fetch(`/api/limit/${selectedCategory}`);
+        const data = await response.json();
+        const categoryLimit = data.category_limit;
+        return categoryLimit;
+    } catch (error) {
+        console.log("Error: " + error);
+    }
+}
+
+async function sendLimit(selectedCategory, newLimit) {
+    try {
+        const response = await fetch('/api/set-limit', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ oldCategory: selectedCategory, limit: newLimit })
+        });
+
+        const data = await response.json();
+
+        return data;
+    } catch (error) {
+        console.log("Error: " + error);
+    }
+}
+
+async function getExpenses(selectedCategory, selectedDate) {
+    try {
+        const response = await fetch(`/api/expenses/${selectedCategory}/${selectedDate}`);
+        const data = await response.json();
+        const expensesSum = data.sum;
+        return expensesSum;
+    } catch (error) {
+        console.log("Error: " + error);
+    }
+}
+
+async function deleteLimit(selectedCategory) {
+    try {
+        const response = await fetch('/api/delete-limit', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ oldCategory: selectedCategory })
+        });
+
+        const data = await response.json();
+
+        return data;
+    } catch (error) {
+        console.log("Error: " + error);
+    }
+}
